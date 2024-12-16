d59644e216f478b4daae1f94c026d3804ac45ab4
pchung <you@example.com>
2024-12-16 15:06:02 +0900

    rm commetn

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: pchung <pchung@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 09:55:52 by pchung            #+#    #+#             */
-/*   Updated: 2024/12/16 13:51:41 by pchung           ###   ########.fr       */
+/*   Updated: 2024/12/16 15:05:54 by pchung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,32 +70,3 @@ double	ft_atof(char *str, char **endptr)
 	}
 	return (nb * sign);
 }
-
-// double	ft_atof(char *str)
-// {
-// 	int i;
-// 	double nb;
-// 	int is_neg;
-// 	double div;
-
-// 	nb = 0;
-// 	div = 0.1;
-// 	is_neg = 1;
-// 	i = skip_space_sign(str, &is_neg);
-// 	while (str[i] && ft_isdigit(str[i]) && str[i] != '.')
-// 	{
-// 		nb = (nb * 10.0) + (str[i] - '0');
-// 		i++;
-// 	}
-// 	if (str[i] == '.')
-// 		i++;
-// 	while (str[i] && ft_isdigit(str[i]))
-// 	{
-// 		nb = nb + ((str[i] - '0') * div);
-// 		div *= 0.1;
-// 		i++;
-// 	}
-// 	if (str[i] && !ft_isdigit(str[i]))
-// 		return (-42); // error
-// 	return (nb * is_neg);
-// }
